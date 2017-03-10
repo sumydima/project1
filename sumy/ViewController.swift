7ff72bbb0ebae874ea3c10695a93eabe88988e6d
Anna Borodina <anniborodina@Annas-MacBook-Pro.local>
2017-03-09 22:30:28 -0800

    Initial Commit

---
+++ sumy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sumy
+//
+//  Created by Anna Borodina on 3/9/17.
+//  Copyright © 2017 Sumy University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
